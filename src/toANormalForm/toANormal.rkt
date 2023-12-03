ef9abfc363d6ded1d4923bcd736bf955a21c406a
Job Hernandez Lara <hj93@protonmail.com>
2023-12-03 00:02:06 -0800

    add template

--- src/toANormalForm/toANormal.rkt
+++ src/toANormalForm/toANormal.rkt
@@ -1,7 +1,39 @@
 #lang racket
 
-(provide to-anormal-form)
+(provide program-to-a-normal-form)
 
 (require "../parser/pyparser.rkt"
-         "../parser/nodes.rkt")
+         "../parser/nodes.rkt"
+         "a-nodes.rkt")
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; this module takes the AST for the parse tree and turns it into
+;;; a-normal-form.
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (program-to-a-normal-form ast)
+  (define (to-normal-form ast)
+    (match ast
+      ...))
+  ...)
+
+(define (a-normalize-assignment ast)
+  ...)
+
+(define (a-normalize-if-expression ast)
+  ...)
+
+(define (a-normalize-while-loop-expression ast)
+  ...)
+
+(define (a-normalize-if-statement ast)
+  ...)
+
+(define (a-normalize-function ast)
+  ...)
+
+(define (atomic-exp? ast)
+  ...)
