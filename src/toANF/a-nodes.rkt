259a1f65dec669a2d116f9db7903937450450d2a
Job Hernandez Lara <hj93@protonmail.com>
2023-12-11 18:31:05 -0800

    rename dir to ANF

---
+++ src/toANF/a-nodes.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(provide (all-defined-out))
+
+(struct atomic (atom) #:transparent)
+
+(struct atomic-assignment (var expr) #:transparent)
+
+(struct atomic-plus (e e2) #:transparent)
+
+(struct atomic-print (e) #:transparent)
